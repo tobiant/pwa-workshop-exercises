8528e07eb3673590fe55de97698a1516b3462710
Leon Möller <lmr@inwerken.de>
2021-11-25 16:46:43 +0100

    inital commit

--- sw.js
+++ sw.js
@@ -1,18 +1,5 @@
 var CACHE_NAME = 'simple-sw-cache-v1';
-var urlsToCache = [
-	'./',
-	'index.html',
-	'sw.js',
-	'manifest.webmanifest',
-	'/assets/icons/apple-touch-icon.png',
-	'/assets/icons/favicon-32x32.png',
-	'/assets/icons/favicon-16x16.png',
-	'/assets/icons/safari-pinned-tab.svg',
-	'/assets/icons/favicon.ico',
-	'/assets/icons/browserconfig.xml',
-	'/assets/icons/android-chrome-192x192.png',
-	'/assets/icons/android-chrome-512x512.png'
-];
+var urlsToCache = ['./'];
 
 self.addEventListener('install', function(event) {
 	// Perform install steps
