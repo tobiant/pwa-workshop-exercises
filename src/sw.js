f918e6aa7fed699d64c4a1ee02c1a4379586df0e
Leon Möller <lmr@inwerken.de>
2021-11-26 13:30:21 +0100

    solution

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,18 @@
 var CACHE_NAME = 'simple-sw-cache-v1';
-var urlsToCache = [];
+var urlsToCache = [
+	'./', // scope when the app gets called via domain - scopes index.html
+	'index.html', // for calls like <domain>.com/index.html
+	'sw.js',
+	'manifest.webmanifest',
+	'/assets/icons/apple-touch-icon.png',
+	'/assets/icons/favicon-32x32.png',
+	'/assets/icons/favicon-16x16.png',
+	'/assets/icons/safari-pinned-tab.svg',
+	'/assets/icons/favicon.ico',
+	'/assets/icons/browserconfig.xml',
+	'/assets/icons/android-chrome-192x192.png',
+	'/assets/icons/android-chrome-512x512.png'
+];
 
 // set event handler for install event, this will be called when the page loads the first time.
 self.addEventListener('install', function(event) {
