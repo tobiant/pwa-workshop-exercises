1c24bbb95a7654fb8a8f35d1dc37193d23ba1278
tge@inwerken.de <tge@inwerken.de>
2022-02-03 11:28:53 +0100

    comments

--- src/sw.js
+++ src/sw.js
@@ -4,11 +4,15 @@ var urlsToCache = [];
 // set event handler for install event, this will be called when the page loads the first time.
 self.addEventListener('install', function(event) {
 	// Perform install steps
-	console.log('Installing resources...');
+	//siehe auch https://developer.mozilla.org/en-US/docs/Web/API/CacheStorage
+	urlsToCache.push("/assets/icons/favicon.ico");
+	// urlsToCache.push("/sw.js"); //muss nicht in den cache aufgenommen werden
+	urlsToCache.push("/index.html");
+	urlsToCache.push("/"); //base scope (localhost)
 	event.waitUntil(
 		caches.open(CACHE_NAME)
 			.then(function(cache) {
-				console.log('Adding defined resources to cache');
+				console.log('Adding defined resources to cache');//hier müssen die ressoucen in den cach geladen werden
 				return cache.addAll(urlsToCache);
 			})
 	);
@@ -18,7 +22,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
 	event.respondWith(
 		caches.match(event.request)
-			.then(function(response) {
+			.then(function(response) { //hier den cache lesen
 					// if there's nothing in cache fall back to network
 					if (response) {
 						return response;
