c30b0ec1a0a0686d2ef914ddf8b3e5a220bd35fb
tge@inwerken.de <tge@inwerken.de>
2022-02-03 15:21:25 +0100

    weiter

--- webapp/sw.js
+++ webapp/sw.js
@@ -1,36 +1,55 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.3.0/workbox-sw.js');
 
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.3.0/workbox-sw.js');
+
 workbox.setConfig({debug: true});
 workbox.precaching.precacheAndRoute([
 	{
 		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap-ui-core.js', revision: null
 	}, {
 		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/ui/core/library-preload.js', revision: null
-	},{
+	}, {
 		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/m/library-preload.js', revision: null
-	},{
+	}, {
 		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/ui/layout/library-preload.js', revision: null
-	},{
+	}, {
 		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/ui/core/date/Gregorian.js', revision: null
-	},{
+	}, {
 		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/m/themes/sap_fiori_3/library.css', revision: null
-	},{
-		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/ui/core/themes/sap_fiori_3/library.css', revision: null
-	},{
-		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/ui/layout/themes/sap_fiori_3/library.css', revision: null
-	},{
-		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/ui/layout/themes/sap_fiori_3/library-parameters.json', revision: null
-	},{
+	}, {
+		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/ui/core/themes/sap_fiori_3/library.css',
+		revision: null
+	}, {
+		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/ui/layout/themes/sap_fiori_3/library.css',
+		revision: null
+	}, {
+		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/ui/layout/themes/sap_fiori_3/library-parameters.json',
+		revision: null
+	}, {
 		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/m/messagebundle.properties', revision: null
-	},{
+	}, {
 		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/ui/unified/library-preload-lazy.js', revision: null
-	},{
+	}, {
 		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/ui/core/messagebundle.properties', revision: null
-	},{
+	}, {
+		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/m/messagebundle.properties', revision: null
+	},
+	{
+		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/ui/model/odata/v2/ODataModel.js', revision: null
+	}, {
+		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/ui/thirdparty/datajs.js', revision: null
+	},
+	{
 		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/ui/layout/messagebundle.properties', revision: null
-	},{
-		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/ui/core/themes/sap_fiori_3/fonts/72-Regular.woff2', revision: null
-	}, ...self.__WB_MANIFEST]);
+	}, {
+		url: 'https://sapui5.hana.ondemand.com/1.96.0/resources/sap/ui/core/themes/sap_fiori_3/fonts/72-Regular.woff2',
+		revision: null
+	},
+	...self.__WB_MANIFEST], {
+	// Ignore all URL parameters.
+	ignoreURLParametersMatching: [/sap.*/]
+});
+
 
 
 // create the backgroundsync queue to save post requests in indexedDB API
